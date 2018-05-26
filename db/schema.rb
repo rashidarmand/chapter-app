161222c2e64398799e1a372b05995578b55f500e
Rashid H Armand <rashidharmand@gmail.com>
2018-05-26 18:09:50 -0400

    added clearance and the associated views

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_26_204224) do
+ActiveRecord::Schema.define(version: 2018_05_26_215507) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -27,6 +27,19 @@ ActiveRecord::Schema.define(version: 2018_05_26_204224) do
     t.integer "book_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email", null: false
+    t.string "encrypted_password", limit: 128, null: false
+    t.string "confirmation_token", limit: 128
+    t.string "remember_token", limit: 128, null: false
+    t.index ["email"], name: "index_users_on_email"
+    t.index ["remember_token"], name: "index_users_on_remember_token"
   end
 
 end
