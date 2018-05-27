1f948c6f16699de8735278e032afd97efc9c49ce
Stephen <stephen.park@live.com>
2018-05-27 19:15:52 -0400

    account form isn't submitting

--- app/controllers/account_controller.rb
+++ app/controllers/account_controller.rb
@@ -5,7 +5,15 @@ class AccountController < ApplicationController
   
       end
 
-    def update
-
+    def create
+        if params[:email]
+          @user.update(email: params[:email])
+          redirect_to account_path
+          flash[:success] = "Email Succesfully Updated"
+        elsif params[:password]
+          @user.update(password: params[:password])
+          redirect_to account_path
+          flash[:success] = "Password Succesfully Updated"
+        end
     end
 end
