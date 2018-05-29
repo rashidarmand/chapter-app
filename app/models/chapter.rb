f638565419358ffe4c2a1b946bc5c5db20b4979c
Rashid H Armand <rashidharmand@gmail.com>
2018-05-29 17:16:43 -0400

    removed searchkick & fixed issue with Faker

--- app/models/chapter.rb
+++ app/models/chapter.rb
@@ -1,5 +1,5 @@
 class Chapter < ApplicationRecord
-  searchkick
+  
   belongs_to :book
   has_many :user_chapters
   has_many :users, through: :user_chapters
@@ -7,4 +7,5 @@ class Chapter < ApplicationRecord
   validates :chapter_order, presence: true
   validates :content, presence: true
   validates :book_id, presence: true
+  
 end
