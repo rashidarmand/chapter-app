b46a3120e4d9f2dc0c18ed319f12fd1586201494
Lauren Tracy <lauren.m.tracy@gmail.com>
2018-05-29 17:49:38 -0400

    updated profile page

--- app/assets/javascripts/main.js
+++
@@ -1,9 +0,0 @@
-$('.toggleMenu').click(function(){
-    $('.sidemenu').addClass("showmenu");
-    $('.toggleMenu').addClass("changeopacity");
-});
-
-$('.cross').click(function(){
-    $('.sidemenu').removeClass("showmenu");
-    $('.toggleMenu').removeClass("changeopacity");
-});
--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,8 @@ Rails.application.routes.draw do
 
   
   root 'home#index'
-  resources :feed
+  resources :profile
   resources :read
+  resources :books
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
