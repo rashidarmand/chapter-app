048ec94e65e8a0a1e4945176e5732edf9490c24e
Rashid H Armand <rashidharmand@gmail.com>
2018-06-01 16:44:59 -0400

    added logic to multiple views & controllers

--- app/controllers/read_controller.rb
+++ app/controllers/read_controller.rb
@@ -1,2 +1,14 @@
 class ReadController < ApplicationController
+  def index
+    # @book = Book.find(params[:id])
+  end
+
+  def show
+    @book = Book.find(params[:id])
+    @chapters = @book.chapters.order(:chapter_order)
+  end
+
+  def chapter
+
+  end
 end
