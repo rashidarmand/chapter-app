fd11ea99060435e200a3da05bc1eaffd0617feab
Rashid H Armand <rashidharmand@gmail.com>
2018-06-03 13:02:47 -0400

    created class instance method to break down chapters into 250 word pages

--- app/models/chapter.rb
+++ app/models/chapter.rb
@@ -12,16 +12,29 @@ class Chapter < ApplicationRecord
     
   end
   
-  def exceed_200_words
-    Chapter.first.content.scan(/\w+/).count
-    if self.content.scan(/\w+/).count > 200
-
-      truncate 
-
-
+  # Check if chapter has more than 250 words in it
+  def long_chapter?
+    if self.content.split(' ').count > 250
+      true
+    else
+      false
     end
-
-
   end
 
+  # Breaks down chapter into an array of arrays. The inner arrays have at most 250 words.
+  def broken_up_chapters
+    content_array = self.content.split(' ')
+    small_chunks_array = []
+    
+    while content_array.length > 250 do
+      small_chunks_array.push(content_array[0...250].join(' '))
+      content_array.shift(250)
+    end
+
+    small_chunks_array.push(content_array[0...250].join(' '))
+
+    return small_chunks_array
+  end
+
+  
 end
