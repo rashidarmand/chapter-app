b1cb1f18e3ff91e2048abbd633e8909b42040d49
Rashid H Armand <rashidharmand@gmail.com>
2018-06-03 20:39:06 -0400

    fixed error

--- app/controllers/read_controller.rb
+++ app/controllers/read_controller.rb
@@ -10,12 +10,12 @@ class ReadController < ApplicationController
 
   def chapter
     @chapter = Chapter.find(params[:chapter_id])
-    if Chapter.find(params[:chapter_id] + 1).book_id == @chapter.book_id 
-      @next_chapter = Chapter.find(params[:chapter_id] + 1)
-    end
-    if Chapter.find(params[:chapter_id] - 1).book_id == @chapter.book_id 
-      @previous_chapter = Chapter.find(params[:chapter_id] - 1)
-    end
+    # if Chapter.find(params[:chapter_id] + 1).book_id == @chapter.book_id 
+    #   @next_chapter = Chapter.find(params[:chapter_id] + 1)
+    # end
+    # if Chapter.find(params[:chapter_id] - 1).book_id == @chapter.book_id 
+    #   @previous_chapter = Chapter.find(params[:chapter_id] - 1)
+    # end
     @chunk_of_chapter = @chapter.broken_up_chapters.paginate(:page => params[:page], :per_page => 1)
   end
 end
