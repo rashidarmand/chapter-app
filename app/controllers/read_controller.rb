dd9d7fe10d4322453bee5fd6c09053bc56108d16
Rashid H Armand <rashidharmand@gmail.com>
2018-06-04 15:50:33 -0400

    added more tweaks

--- app/controllers/read_controller.rb
+++ app/controllers/read_controller.rb
@@ -9,22 +9,20 @@ class ReadController < ApplicationController
 
   def chapter
     @chapter = Chapter.find(params[:chapter_id])
-    # if Chapter.find(params[:chapter_id] + 1).book_id == @chapter.book_id 
-    #   @next_chapter = Chapter.find(params[:chapter_id] + 1)
-    # end
-    # if Chapter.find(params[:chapter_id] - 1).book_id == @chapter.book_id 
-    #   @previous_chapter = Chapter.find(params[:chapter_id] - 1)
-    # end
+   
     @chunk_of_chapter = @chapter.broken_up_chapters.paginate(:page => params[:page], :per_page => 1)
    
-    if Chapter.find(@chapter.id + 1).book_id == @chapter.book_id 
+    if Chapter.find(@chapter.id + 1).book_id == @chapter.book_id && Chapter.find(@chapter.id + 1).chapter_order == @chapter.chapter_order + 1
       @next_chapter = Chapter.find(@chapter.id + 1)
     end
-    # if Chapter.find(@chapter.id - 1).book_id == @chapter.book_id
-    #   if @chapter.id != 1 
-    #     @previous_chapter = Chapter.find(@chapter.id - 1)
-    #   end
-    # end
+
+    if @chapter != @chapter.book.chapters.first && Chapter.find(@chapter.id - 1).book_id == @chapter.book_id && Chapter.find(@chapter.id - 1).chapter_order == @chapter.chapter_order - 1
+      @previous_chapter = Chapter.find(@chapter.id - 1)
+    end
+
+    if @chapter == @chapter.book.chapters.last && current_user.books.include?(@chapter.book)
+      @user_book = UserBook.find_by(user_id: current_user.id, book_id: @chapter.book.id)
+    end
 
   end
 end
