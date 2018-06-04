247e467543ce933629f9b8618f995cb347885357
jmiles90 <jmilescespedes@gmail.com>
2018-06-04 13:20:04 -0400

    email jobs & schedulers tested working using seconds instead of minutes

--- app/jobs/reg_batch_email_job.rb
+++ app/jobs/reg_batch_email_job.rb
@@ -1,7 +1,12 @@
 class RegBatchEmailJob < ApplicationJob
   queue_as :default
 
-  def perform(*args)
-    # Do something later
+  def perform(data)
+    
+        NextChapterMailer.next_chapter(data[0],data[1]).deliver
+
+      
+    
   end
 end
+    
--- app/mailers/next_chapter_mailer.rb
+++ app/mailers/next_chapter_mailer.rb
@@ -1,2 +1,11 @@
 class NextChapterMailer < ApplicationMailer
+    default from: "chapterbookworms@gmail.com"
+
+    def next_chapter(user,chapter)
+        @user = user
+        @chapter = chapter
+        mail(to: @user.email, subject: 'Your next Chapter!')
+      end
+    
+    
 end
