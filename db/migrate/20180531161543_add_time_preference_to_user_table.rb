be6d9fcde00000102aff0d5ddb89b5f56085d879
Rashid H Armand <rashidharmand@gmail.com>
2018-06-08 16:19:57 -0400

    migration issue

--- db/migrate/20180531161543_add_time_preference_to_user_table.rb
+++ db/migrate/20180531161543_add_time_preference_to_user_table.rb
@@ -1,5 +1,7 @@
 class AddTimePreferenceToUserTable < ActiveRecord::Migration[5.2]
   def change
-    add_column :users, :time_preference, :string
+    add_column :users, :time_preference, :integer
+    
+    add_column :users, :time_zone, :integer
   end
 end
--- db/migrate/20180531181449_add_time_zone_to_user_table.rb
+++
@@ -1,7 +0,0 @@
-class AddTimeZoneToUserTable < ActiveRecord::Migration[5.2]
-  def change
-    change_column :users, :time_preference, :integer
-
-    add_column :users, :time_zone, :integer
-  end
-end
