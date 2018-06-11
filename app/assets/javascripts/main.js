b59ccf87a5e7b5c8709518631415a316e6af210b
Rashid H Armand <rashidharmand@gmail.com>
2018-06-10 23:03:21 -0400

    converted jquery to vanilla js

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -3,69 +3,53 @@
 
 
 document.addEventListener("turbolinks:load", function(){
-    
-    let flashPanel = document.querySelector(".flash")
-    // flash timeout
-    window.setTimeout(() => {
-        flashPanel.classList.add("hide")
-      }, 3000);
+	
+	let flashPanel = document.querySelector(".flash")
+	// flash timeout
+	window.setTimeout(() => {
+		flashPanel.classList.add("hide")
+	  }, 3000);
 
-    // sepia setting
-    $(document).ready(function(){
-        $("#sepia").click(function(){
-            // $("#readPage").css({"background-color": "#d8d6bf", "color": "#000"});
-            $("body").css({"background-color": "#d8d6bf", "color": "#000"});
-            $(".fas.fa-search-plus").css({"color": "#000"});
-            $(".fas.fa-search-minus").css({"color": "#000"});
-            $(".far.fa-bookmark").css({"color": "#000"});
-        });
-    });
-    
-    // black setting
-    $(document).ready(function(){
-        $("#black").click(function(){
-            // $("#readPage").css({"background-color": "#000", "color": "#fff"});
-            $("body").css({"background-color": "#000", "color": "#fff"});
-            $(".fas.fa-search-plus").css({"color": "#fff"});
-            $(".fas.fa-search-minus").css({"color": "#fff"});
-            $(".far.fa-bookmark").css({"color": "#fff"});
-        });
-    });
-    
-    // white setting
-    $(document).ready(function(){
-        $("#white").click(function(){
-            // $("#readPage").css({"background-color": "#fff", "color": "#000"});
-            $("body").css({"background-color": "#fff", "color": "#000"});
-            $(".fas.fa-search-plus").css({"color": "#000"});
-            $(".fas.fa-search-minus").css({"color": "#000"});
-            $(".far.fa-bookmark").css({"color": "#000"});
-        });
-    });
+	// sepia setting
+	document.querySelector('#sepia').addEventListener('click', ()=>{
+		document.querySelector('body').style.cssText = "background-color: #d8d6bf; color: #000;";
+		document.querySelector('.fas.fa-search-plus').style.cssText = "color: #000;";
+		document.querySelector('.fas.fa-search-minus').style.cssText = "color: #000;";
+		document.querySelector('.far.fa-bookmark').style.cssText = "color: #000;";
+	});
+	
+	
+	// black setting
+	document.querySelector('#black').addEventListener('click', ()=>{
+		document.querySelector('body').style.cssText = "background-color: #000; color: #fff;";
+		document.querySelector('.fas.fa-search-plus').style.cssText = "color: #fff;";
+		document.querySelector('.fas.fa-search-minus').style.cssText = "color: #fff;";
+		document.querySelector('.far.fa-bookmark').style.cssText = "color: #fff;";
+	});
+	
+	// white setting
+	document.querySelector('#white').addEventListener('click', ()=>{
+		document.querySelector('body').style.cssText = "background-color: #fff; color: #000;";
+		document.querySelector('.fas.fa-search-plus').style.cssText = "color: #000;";
+		document.querySelector('.fas.fa-search-minus').style.cssText = "color: #000;";
+		document.querySelector('.far.fa-bookmark').style.cssText = "color: #000;";
+	});
 
-    // font size settings
-    $(document).ready(function(){
-        var originalSize = $('#textBox').css('font-size');
+	// font size settings
+	// // increase 
+	document.querySelector('#zoom').addEventListener('click', ()=>{
+		let currentFontSize = window.getComputedStyle(document.querySelector('#textBox')).fontSize;
+		let currentSize = parseFloat(currentFontSize) * 1.2;
+		document.querySelector('#textBox').style.cssText = `font-size: ${currentSize}px;`;
+	});
+  
+	//  // decrease
+	document.querySelector('#minus').addEventListener('click', ()=>{
+		let currentFontSize = window.getComputedStyle(document.querySelector('#textBox')).fontSize;
+		let currentSize = parseFloat(currentFontSize) * 0.8;
+		document.querySelector('#textBox').style.cssText = `font-size: ${currentSize}px;`;
+	});
 
-    // increase 
-        $("#zoom").click(function(){
-         var currentSize = $('#textBox').css('font-size');
-         var currentSize = parseFloat(currentSize)*1.2;
-         $('#textBox').css('font-size', currentSize);
-  
-        return false;
-     });
-  
-     // decrease
-     $("#minus").click(function(){
-        var currentFontSize = $('#textBox').css('font-size');
-        var currentSize = $('#textBox').css('font-size');
-        var currentSize = parseFloat(currentSize)*0.8;
-        $('#textBox').css('font-size', currentSize);
-  
-        return false;
-     });
-  });
 
 // Notice Timeout
 
@@ -75,44 +59,44 @@ document.addEventListener("turbolinks:load", function(){
 
 
 function bookProgress(){
-    booksRead = parseInt(document.getElementById("books-read").innerHTML);
-    userRank = document.getElementById("user-rank");
-    progressBar = document.getElementById("progress-bar")
-    if(booksRead < 4){
-        userRank.innerHTML = "Caterpillar";
-        if (booksRead===1){
-            progressBar.setAttribute("style",`width:${33}%`)
-        }
-        else if(booksRead === 2){
-            progressBar.setAttribute("style",`width:${66}%`)
-        }
-        else if (booksRead ===3){
+	booksRead = parseInt(document.getElementById("books-read").innerHTML);
+	userRank = document.getElementById("user-rank");
+	progressBar = document.getElementById("progress-bar")
+	if(booksRead < 4){
+		userRank.innerHTML = "Caterpillar";
+		if (booksRead===1){
+			progressBar.setAttribute("style",`width:${33}%`)
+		}
+		else if(booksRead === 2){
+			progressBar.setAttribute("style",`width:${66}%`)
+		}
+		else if (booksRead ===3){
 
-            progressBar.setAttribute("style",`width:${92}%`)
-        }
-    }
-    else if (booksRead > 3 && booksRead < 8){
-        userRank.innerHTML = "Cocoon";
-        if(booksRead ===4 ){
-            progressBar.setAttribute("style",`width:${9}%`)
-        }
-        else if (booksRead===5){
-            progressBar.setAttribute("style",`width:${33}%`)
-        }
-        else if(booksRead === 6){
-            progressBar.setAttribute("style",`width:${66}%`)
-        }
-        else if (booksRead ===7){
+			progressBar.setAttribute("style",`width:${92}%`)
+		}
+	}
+	else if (booksRead > 3 && booksRead < 8){
+		userRank.innerHTML = "Cocoon";
+		if(booksRead ===4 ){
+			progressBar.setAttribute("style",`width:${9}%`)
+		}
+		else if (booksRead===5){
+			progressBar.setAttribute("style",`width:${33}%`)
+		}
+		else if(booksRead === 6){
+			progressBar.setAttribute("style",`width:${66}%`)
+		}
+		else if (booksRead ===7){
 
-            progressBar.setAttribute("style",`width:${92}%`)
-        }
-    }
-    else if (booksRead > 7){
-      userRank.innerHTML = "Butterfly";
-      elements = document.getElementsByClassName("book-progress-bar");
-      for(let i = 0; i < elements.length; i++){
-          elements[i].classList.add("hidden");
-      }
-    }  
+			progressBar.setAttribute("style",`width:${92}%`)
+		}
+	}
+	else if (booksRead > 7){
+	  userRank.innerHTML = "Butterfly";
+	  elements = document.getElementsByClassName("book-progress-bar");
+	  for(let i = 0; i < elements.length; i++){
+		  elements[i].classList.add("hidden");
+	  }
+	}  
 }
 
